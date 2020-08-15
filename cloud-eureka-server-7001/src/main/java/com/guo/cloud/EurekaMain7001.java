8a71d34af5d05aef4402564212ba3e8ed31e03cf
zichunguo <guozichunvip@163.com>
2020-08-15 08:30:29 +0800

    添加 Eureka 测试

--- cloud-eureka-server-7001/src/main/java/com/guo/cloud/EurekaMain7001.java
+++ cloud-eureka-server-7001/src/main/java/com/guo/cloud/EurekaMain7001.java
@@ -2,16 +2,16 @@ package com.guo.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author guo
  * @date 2020/8/14
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class ManageMain80 {
+@EnableEurekaServer
+public class EurekaMain7001 {
 	public static void main(String[] args) {
-		SpringApplication.run(ManageMain80.class, args);
+		SpringApplication.run(EurekaMain7001.class, args);
 	}
 }
--- cloud-provider-user-8001/src/main/java/com/guo/cloud/UserMain8001.java
+++ cloud-provider-user-8001/src/main/java/com/guo/cloud/UserMain8001.java
@@ -2,12 +2,14 @@ package com.guo.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author guo
  * @date 2020/8/14
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class UserMain8001 {
 	public static void main(String[] args) {
 		SpringApplication.run(UserMain8001.class, args);
