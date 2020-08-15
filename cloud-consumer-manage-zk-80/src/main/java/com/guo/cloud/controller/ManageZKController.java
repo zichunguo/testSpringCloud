85dd8add56f8bd53520ffe7a9aaaf9c225c3f818
zichunguo <guozichunvip@163.com>
2020-08-15 20:02:36 +0800

    修改 Zookeeper 作为注册中心的测试

--- cloud-consumer-manage-zk-80/src/main/java/com/guo/cloud/controller/ManageZKController.java
+++ cloud-consumer-manage-zk-80/src/main/java/com/guo/cloud/controller/ManageZKController.java
@@ -11,14 +11,14 @@ import javax.annotation.Resource;
  * @date 2020/8/15
  */
 @RestController
-public class TestZKController {
+public class ManageZKController {
 
 	public static final String INVOKE_URL = "http://cloud-user-service";
 
 	@Resource
 	private RestTemplate restTemplate;
 
-	@GetMapping("/consumer/test/zk")
+	@GetMapping("/consumer/user/zk")
 	public String testZKInfo() {
 		return restTemplate.getForObject(INVOKE_URL + "/user/zk", String.class);
 	}
