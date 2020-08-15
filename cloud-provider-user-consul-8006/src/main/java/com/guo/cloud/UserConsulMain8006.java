9b135dcc40d83fef37d58eaf301b76759b323dd9
zichunguo <guozichunvip@163.com>
2020-08-16 00:26:10 +0800

    修改 Eureka 地址

--- cloud-provider-user-consul-8006/src/main/java/com/guo/cloud/UserConsulMain8006.java
+++ cloud-provider-user-consul-8006/src/main/java/com/guo/cloud/UserConsulMain8006.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @date 2020/8/15
  */
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableDiscoveryClient	// 该注解用于向使用 consul 或者 zookeeper 作为注册中心时注册服务
 public class UserConsulMain8006 {
 	public static void main(String[] args) {
 		SpringApplication.run(UserConsulMain8006.class, args);
