d0193c124dbc4bcba4b91f4b7d58cbecf430f096
zichunguo <guozichunvip@163.com>
2020-08-20 23:19:26 +0800

    修改 sleuth zipkin 说明注释

--- cloud-sleuth-provider-8701/src/main/java/com/guo/cloud/controller/SleuthProviderController.java
+++ cloud-sleuth-provider-8701/src/main/java/com/guo/cloud/controller/SleuthProviderController.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 测试 sleuth + zipkin 分布式请求链路跟踪
+ * 测试需要先启动 zipkin
+ * zipkin jar 下载地址：https://dl.bintray.com/openzipkin/maven/io/zipkin/java/zipkin-server/
+ * 		或在 zipkin 的 github 中下载最新版本：https://github.com/openzipkin/zipkin，找到 "latest released server" 点击下载
+ * 		或使用 docker 启动
+ * 运行：java -jar zipkin-server-2.12.9-exec.jar
+ * 访问：http://localhost:9411/zipkin
  * @author guo
  * @date 2020/8/20
  */
